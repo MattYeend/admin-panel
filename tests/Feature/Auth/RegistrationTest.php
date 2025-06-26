295ef624cd72c68703bf63ff680b70fe66b5c165
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-06-26 15:12:46 +0100

    #5 Update to registration test

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -14,14 +14,13 @@ test('new users can register', function () {
     $response = $this->post('/register', [
         'title' => 'Mr',
         'name' => 'Test User',
-        'slug' => Str::slug('Test User'),
+        'slug' => Str::slug('Test User'), // Optional: see note below
         'email' => 'test@example.com',
         'password' => 'password',
         'password_confirmation' => 'password',
         'role' => 3
     ]);
 
-    $response->assertStatus(200);
-    $this->assertAuthenticated();
     $response->assertRedirect(route('dashboard', absolute: false));
+    $this->assertAuthenticated();
 });
